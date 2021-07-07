0809685f503e94238c7fa37916dc9d67825d9534
QsVlEANN <evgeniy.radchin@stud.onu.edu.ua>
2021-07-07 17:36:06 +0300

    5 - add some tests for linkedlist

--- src/main/java/realise_linkedlist/Test.java
+++ src/main/java/realise_linkedlist/Test.java
@@ -1,9 +1,19 @@
 package realise_linkedlist;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
         MyLinkedList<Integer> linkList = new MyLinkedList<>();
         linkList.add(5);
         System.out.println(linkList.contains(5));
+        linkList.add(10);
+        linkList.add(7);
+        linkList.add(9);
+        linkList.add(4);
+        System.out.println(Arrays.toString(linkList.toArray()));
+        linkList.set(0, 1000);
+        linkList.removeLast();
+        System.out.println(Arrays.toString(linkList.toArray()));
     }
 }
